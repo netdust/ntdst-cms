4f7d6bc862922fa09cb552f646a03decd6dae9d5
netdust <stefan@netdust.be>
2016-02-06 14:25:41 +0100

    update helper metthods for new twig API

--- src/www/admin/system/api/Extensions/ApiHelpers.php
+++ src/www/admin/system/api/Extensions/ApiHelpers.php
@@ -16,8 +16,11 @@ class ApiHelpers extends \Slim\Views\TwigExtension
         );
 
         return array(
-            new \Twig_SimpleFunction('page_*', array($this, 'page_handler'), $options ),
-            new \Twig_SimpleFunction('__*', array($this, 'util_handler'), $options ),
+            new \Twig_SimpleFunction('_s__*', array($this, 'site_handler'), $options ),
+            new \Twig_SimpleFunction('_l__', array($this, 'location_handler'), $options ),
+            new \Twig_SimpleFunction('_p__', array($this, 'page_handler'), $options ),
+
+            new \Twig_SimpleFunction('__', array($this, 'util_handler'), $options ),
             new \Twig_SimpleFunction('_*', array($this, 'php_handler'), $options )
 
         );
@@ -37,6 +40,18 @@ class ApiHelpers extends \Slim\Views\TwigExtension
     {
         $param = str_replace( '_', '-', $param );
         return call_user_func_array(array('\helpers\Page', $param), is_array($arg)?$arg:array($arg));
+    }
+
+    public function location_handler( $param, $arg='' )
+    {
+        $param = str_replace( '_', '-', $param );
+        return call_user_func_array(array('\helpers\Location', $param), is_array($arg)?$arg:array($arg));
+    }
+
+    public function site_handler( $param, $arg='' )
+    {
+        $param = str_replace( '_', '-', $param );
+        return call_user_func_array(array('\helpers\Site', $param), is_array($arg)?$arg:array($arg));
     }
 
 
